506b84894af0d0a0db3fd50065105c483931453e
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-10-25 17:01:03 -0500

    Formulario de solicitud de viáticos y transporte- Construcción del fomulario de vÃiaticos

--- apps/static/assets/js/myScripts/nuevaSolicitud.js
+++ apps/static/assets/js/myScripts/nuevaSolicitud.js
@@ -1,8 +1,48 @@
-const nuevaSolicitud = document.getElementById('nuevaSolicitud');
+const formNuevaSolicitud = document.querySelector('#formNuevaSolicitud');
+const selectTipoSolicitud = document.querySelector('#selectTipoSolicitud')
+const btnNuevaSolicitud = document.querySelector('#BotonNuevaSolicitud');
+const contenedorFormViaticos = document.querySelector('#contenedorFormViaticos');
+const contenedorFormAnticipo= document.querySelector('#contenedorFormAnticipo'); 
+const contenedorFormReembolso = document.querySelector('#contenedorFormReembolso'); 
 
-nuevaSolicitud.addEventListener('click', muestraFormulario);
 
-function muestraFormulario(){
-    console.log("click");
-    
+btnNuevaSolicitud.addEventListener('click', muestraOcultaFormulario);
+selectTipoSolicitud.addEventListener('change',eligeTipoSolicitud )
+
+
+function muestraOcultaFormulario(){  
+    formDisplay=formNuevaSolicitud.style.display;
+    if (formDisplay == "none") {
+        formNuevaSolicitud.style.display = "block";
+    }
+    else {
+        formNuevaSolicitud.style.display = "none";
+    }
+   
+}
+
+function eligeTipoSolicitud(){
+    console.log("El tipo de solicitud es:", selectTipoSolicitud.value);
+    switch(selectTipoSolicitud.value) {
+        case 'Viáticos':
+            contenedorFormViaticos.style.display = 'block';
+            contenedorFormAnticipo.style.display = 'none';
+            contenedorFormReembolso.style.display = 'none';
+            break;
+        case 'Anticipo':
+            contenedorFormViaticos.style.display = 'none';
+            contenedorFormAnticipo.style.display = 'block';
+            contenedorFormReembolso.style.display = 'none';
+            break;
+        case 'Reembolso':
+            contenedorFormViaticos.style.display = 'none';
+            contenedorFormAnticipo.style.display = 'none';
+            contenedorFormReembolso.style.display = 'block';
+            break;
+        default:
+            contenedorFormViaticos.style.display = 'none';
+            contenedorFormAnticipo.style.display = 'none';
+            contenedorFormReembolso.style.display = 'none';
+    }
+
 }

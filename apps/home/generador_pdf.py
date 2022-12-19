f2a622b162250efd003d7eb3fc5955c7fef293b3
jlrueda211088@gmail.com <jlrueda211088@gmail.com>
2022-12-19 15:19:11 -0500

    Modificar formato de pdf para agregar beneficiario cuando es solicitud de reeembolso.

--- apps/home/generador_pdf.py
+++ apps/home/generador_pdf.py
@@ -25,88 +25,115 @@ class Solicitud_pdf(FPDF):
         
         self.set_font('Arial', 'B', 10)
         self.set_fill_color(118, 170, 219) #76aadb
-        self.cell(40,10, f'Regional',1, 0, 'C', True)
-        self.cell(40,10, f'Estado',1, 0, 'C', True)
-        self.cell(70,10, f'Colaborador',1,0, 'C', True)
-        self.cell(40,10, f'Valor total',1, 1, 'C', True)
+        self.cell(40,5, f'Regional',1, 0, 'C', True)
+        self.cell(40,5, f'Estado',1, 0, 'C', True)
+        self.cell(70,5, f'Colaborador',1,0, 'C', True)
+        self.cell(40,5, f'Valor total',1, 1, 'C', True)
 
         self.set_font('Arial', '', 10)
-        self.cell(40,10, f'{self.solicitud.regional}',1, 0, 'C')
-        self.cell(40,10, f'{self.solicitud.estado}',1, 0, 'C')
-        self.cell(70,10, f'{self.solicitud.colaborador}',1,0, 'C')
-        self.cell(40,10, f'$ {self.solicitud.valor_total}',1, 1,'C')
+        self.cell(40,5, f'{self.solicitud.regional}',1, 0, 'C')
+        self.cell(40,5, f'{self.solicitud.estado}',1, 0, 'C')
+        self.cell(70,5, f'{self.solicitud.colaborador}',1,0, 'C')
+        self.cell(40,5, f'$ {self.solicitud.valor_total}',1, 1,'C')
 
         self.set_font('Arial', 'B', 10)
-        self.cell(190, 10, 'Observaciones:', 1, 1, 'C')
+        self.cell(190, 5, 'Observaciones:', 1, 1, 'C')
         self.set_font('Arial', '', 10)
         self.cell(190,10, self.solicitud.observaciones,1, 1, 'C')
+        # Solicitud asociada.
+
+        if self.solicitud.solicitud_asociada:
+            self.set_font('Arial', 'B', 10)
+            self.set_fill_color(118, 170, 219) #76aadb
+            self.cell(190, 5, f'Solicitud Asociada: {self.solicitud.solicitud_asociada.id} - {self.solicitud.solicitud_asociada.operacion.operacion} ', 1, 1, 'C',True)
+            self.cell(40,5, f'Fecha solicitud',1, 0, 'C')
+            self.cell(40,5, f'Valor',1, 0, 'C')
+            self.cell(110,5, f'Observaciones',1, 1, 'C')
+            self.set_font('Arial', '', 10)
+            self.cell(40,5, f'{str(self.solicitud.solicitud_asociada.fecha)[0:10]}',1, 0, 'C')
+            self.cell(40,5, f'{self.solicitud.solicitud_asociada.valor_total}',1, 0, 'C')
+            self.cell(110,5, f'{self.solicitud.solicitud_asociada.observaciones}',1, 1, 'C')
+            self.ln()
+
         
     def encabezado_actividad(self, id_actividad):
         self.set_font('Arial', 'B', 10)
         self.set_fill_color(118, 170, 219) #76aadb
-        self.cell(190, 10, f'ACTIVIDAD #{id_actividad} ', 1, 1, 'C',True)
-        self.cell(40,10, f'Fecha actividad',1, 0, 'C')
-        self.cell(60,10, f'Proyecto',1,0, 'C')
-        self.cell(50,10, f'Minicipio',1,0, 'C')
-        self.cell(40,10, f'Valor',1, 1,'C')
+        self.cell(190, 5, f'ACTIVIDAD #{id_actividad} ', 1, 1, 'C',True)
+        self.cell(40,5, f'Fecha actividad',1, 0, 'C')
+        self.cell(60,5, f'Proyecto',1,0, 'C')
+        self.cell(50,5, f'Minicipio',1,0, 'C')
+        self.cell(40,5, f'Valor',1, 1,'C')
 
     def actividad(self, actividad):
         self.set_font('Arial', '', 10)
-        self.cell(40,10, f'{actividad.fecha_actividad}',1, 0, 'C')
-        self.cell(60,10, f'{actividad.proyecto}',1,0, 'C')
-        self.cell(50,10, f'{actividad.municipio}',1,0, 'C')
-        self.cell(40,10, f'$ {actividad.valor}',1, 1,'C')
+        self.cell(40,5, f'{actividad.fecha_actividad}',1, 0, 'C')
+        self.cell(60,5, f'{actividad.proyecto}',1,0, 'C')
+        self.cell(50,5, f'{actividad.municipio}',1,0, 'C')
+        self.cell(40,5, f'$ {actividad.valor}',1, 1,'C')
+        
+        if actividad.beneficiario:
+            self.set_font('Arial', 'B', 10)
+            self.cell(40,5, f'Id',1, 0, 'C')
+            self.cell(60,5, f'Tipo_id',1,0, 'C')
+            self.cell(90,5, f'Beneficiario',1,1, 'C')
+            self.set_font('Arial', '', 10)
+            self.cell(40,5, f'{actividad.beneficiario.beneficiario}',1, 0, 'C')
+            self.cell(60,5, f'{actividad.beneficiario.tipo_id_beneficiario}',1,0, 'C')
+            self.cell(90,5, f'{actividad.beneficiario.nombre}',1,1, 'C')
+        
         self.set_font('Arial', 'B', 10)
-        self.cell(190,10, f'Descripción actividad',1, 1,'C')
+        self.cell(190,5, f'Descripción actividad',1, 1,'C')
         self.set_font('Arial', '', 10)
         self.multi_cell(190,10, f'{actividad.descripcion}',1, align='C')
-
+        
+            
     def encabezado_ruta_viatico(self):
         self.set_fill_color(118, 170, 219) #76aadb
         self.set_font('Arial', 'B', 10)
-        self.cell(190, 10, f'Rutas de viáticos', 1, 1, 'C',True)
-        self.cell(35,10, f'Origen',1, 0, 'C')
-        self.cell(35,10, f'Destino',1, 0,'C')
-        self.cell(20,10, f'Fecha Ini',1,0, 'C')
-        self.cell(20,10, f'Fecha Fin',1,0, 'C')
-        self.cell(10,10, f'Dias',1,0, 'C')
-        self.cell(10,10, f'P',1,0, 'C')
-        self.cell(25,10, f'Transporte',1,0, 'C')
-        self.cell(35,10, f'Viático',1,1, 'C')
+        self.cell(190, 5, f'Rutas de viáticos', 1, 1, 'C',True)
+        self.cell(35,5, f'Origen',1, 0, 'C')
+        self.cell(35,5, f'Destino',1, 0,'C')
+        self.cell(20,5, f'Fecha Ini',1,0, 'C')
+        self.cell(20,5, f'Fecha Fin',1,0, 'C')
+        self.cell(10,5, f'Dias',1,0, 'C')
+        self.cell(10,5, f'P',1,0, 'C')
+        self.cell(25,5, f'Transporte',1,0, 'C')
+        self.cell(35,5, f'Viático',1,1, 'C')
         # self.cell(10,10, f'E',1,1, 'C')
 
     def ruta_viatico(self, ruta):
 
         self.set_font('Arial', '', 10)
-        self.cell(35,10, f'{ruta.origen}',1, 0, 'C')
-        self.cell(35,10, f'{ruta.destino}',1, 0,'C')
-        self.cell(20,10, f'{ruta.fecha_inicial}',1,0, 'C')
-        self.cell(20,10, f'{ruta.fecha_final}',1,0, 'C')
-        self.cell(10,10, f'{ruta.dias_viaje}',1,0, 'C')
-        self.cell(10,10, f'{ruta.pernoctar}',1,0, 'C')
-        self.cell(25,10, f'$ {ruta.transporte}',1,0, 'C')
-        self.cell(35,10, f'$ {ruta.viatico}',1,1, 'C')
+        self.cell(35,5, f'{ruta.origen}',1, 0, 'C')
+        self.cell(35,5, f'{ruta.destino}',1, 0,'C')
+        self.cell(20,5, f'{ruta.fecha_inicial}',1,0, 'C')
+        self.cell(20,5, f'{ruta.fecha_final}',1,0, 'C')
+        self.cell(10,5, f'{ruta.dias_viaje}',1,0, 'C')
+        self.cell(10,5, f'{ruta.pernoctar}',1,0, 'C')
+        self.cell(25,5, f'$ {ruta.transporte}',1,0, 'C')
+        self.cell(35,5, f'$ {ruta.viatico}',1,1, 'C')
         # self.cell(10,10, f'{ruta.estado}',1,1, 'C')
 
 
     def encabezado_gastos_adicionales(self):
         self.set_fill_color(118, 170, 219) #76aadb
         self.set_font('Arial', 'B', 10)
-        self.cell(190, 10, f'Gastos adicionales', 1, 1, 'C',True)
-        self.cell(20,10, f'#',1, 0, 'C')
-        self.cell(50,10, f'Tipo',1, 0, 'C')
-        self.cell(70,10, f'Lugar',1,0, 'C')
-        self.cell(50,10, f'Valor',1,1, 'C')
+        self.cell(190, 5, f'Gastos adicionales', 1, 1, 'C',True)
+        self.cell(20,5, f'#',1, 0, 'C')
+        self.cell(50,5, f'Tipo',1, 0, 'C')
+        self.cell(70,5, f'Lugar',1,0, 'C')
+        self.cell(50,5, f'Valor',1,1, 'C')
         
 
     def gasto_adicional(self, adicional, num_adicional):
         self.set_font('Arial', '', 10)
-        self.cell(20,10, f'{num_adicional}',1, 0, 'C',fill=True)
-        self.cell(50,10, f'{adicional.tipo}',1, 0, 'C')
-        self.cell(70,10, f'{adicional.lugar}',1,0, 'C')
-        self.cell(50,10, f'$ {adicional.valor}',1,1, 'C')
+        self.cell(20,5, f'{num_adicional}',1, 0, 'C',fill=True)
+        self.cell(50,5, f'{adicional.tipo}',1, 0, 'C')
+        self.cell(70,5, f'{adicional.lugar}',1,0, 'C')
+        self.cell(50,5, f'$ {adicional.valor}',1,1, 'C')
         self.set_font('Arial', 'B', 10)
-        self.cell(190,10, f'Descripción',1, 1,'C')
+        self.cell(190,5, f'Descripción',1, 1,'C')
         self.set_font('Arial', '', 10)
         self.multi_cell(190,10, f'{adicional.descripcion}',1, align='C')
         
